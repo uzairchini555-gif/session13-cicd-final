679b831e153f5752c80e5eb676e6ef8d6e17d07a
EC2 Default User <ec2-user@ip-10-0-1-138.ap-southeast-2.compute.internal>
2026-01-22 19:57:24 +0000

    trying CI pipeline

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-	return "session 13 App version 1.1 - deployed at 2026-01-22 21:35"
+	return "session 13 App version 1.1 - ae kash madiney mein mjhey maut yun ae"
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000)
 
